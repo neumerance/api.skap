a46098df89383013f28d467ecc549137642f4cbe
JonJon <neumerance@live.com>
2024-05-05 08:01:10 +0800

    ad rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# ApplicationController serves as the parent class for all controllers in the application.
+# It provides common functionality and configurations that are shared across controllers.
 class ApplicationController < ActionController::API
+  def info
+    logo = File.read(Rails.root.join('app/views/layouts/ascii-logi.txt'))
+
+    render plain: logo
+  end
 end
