5fe1180950f446471ec2604c27a43efdac22d59c
JonJon <neumerance@live.com>
2024-05-05 08:49:17 +0800

    remove force ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.production?
-    constraints(:protocol => "http://") do
-      match "/(*path)" => redirect { |params, request|
-        "https://#{request.host_with_port}/#{params[:path]}"
-      }
-    end
-  end
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
